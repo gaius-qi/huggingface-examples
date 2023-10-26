7c2102c80d25c20659fbe5a22a14dc3f690b1459
Gaius <gaius.qi@gmail.com>
2023-10-26 23:26:39 +0800

    feat: adapter
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- hf_hub_download_dragonfly.py
+++ hf_hub_download_dragonfly.py
@@ -1,2 +1,22 @@
+import requests
+from huggingface_hub import configure_http_backend
 from huggingface_hub import hf_hub_download
-hf_hub_download(endpoint="http://huggingface.co", repo_id="gaius-qi/dragonfly", filename="README.md", proxies={'http': 'http://127.0.0.1:65001'})
+from requests.adapters import HTTPAdapter
+
+class MyAdapter(HTTPAdapter):
+    def get_connection(self, url, proxies=None):
+        if url.startswith('https://'):
+            url = url.replace('https://', 'http://', 1)
+        return super().get_connection(url, proxies)
+
+# Create a factory function that returns a Session with configured proxies
+def backend_factory() -> requests.Session:
+    session = requests.Session()
+    # session.mount('http://', MyAdapter())
+    session.mount('https://', MyAdapter())
+    return session
+
+# Set it as the default session factory
+configure_http_backend(backend_factory=backend_factory)
+
+hf_hub_download(endpoint='http://huggingface.co', repo_id="timm/mobilenetv3_large_100.ra_in1k", filename="model.safetensors", proxies={'http': 'http://127.0.0.1:65001'})
--- request.py
+++ request.py
@@ -1,3 +1,15 @@
 import requests
-r = requests.request(method="GET", url="http://huggingface.co")
+from requests.adapters import HTTPAdapter
+
+class MyAdapter(HTTPAdapter):
+    def request_url(self, request, proxies):
+        if request.url.startswith('https://'):
+            url = request.url.replace('https://', 'http://', 1)
+            print("Request URL: " + url)
+        return request.url
+
+s = requests.Session()
+s.mount('https://', MyAdapter())
+r = s.request(method="GET", url="https://baidu.com")
 print("Status Code: " + str(r.status_code))
+print("Response Content: " + r.text)
