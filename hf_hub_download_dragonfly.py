853a35878d41aec0ee6083e5ca6fe3b18b91fb5e
Gaius <gaius.qi@gmail.com>
2023-10-27 15:05:39 +0800

    feat: dragonfly
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- hf_hub_download_dragonfly.py
+++ hf_hub_download_dragonfly.py
@@ -1,22 +1,34 @@
 import requests
-from huggingface_hub import configure_http_backend
-from huggingface_hub import hf_hub_download
 from requests.adapters import HTTPAdapter
+from urllib.parse import urlparse
+from huggingface_hub import hf_hub_download
+from huggingface_hub import configure_http_backend
 
-class MyAdapter(HTTPAdapter):
+class DragonflyAdapter(HTTPAdapter):
     def get_connection(self, url, proxies=None):
-        if url.startswith('https://'):
-            url = url.replace('https://', 'http://', 1)
+        # Change the schema of the LFS request to download large files from https:// to http://,
+        # so that Dragonfly HTTP proxy can be used.
+        if url.startswith('https://cdn-lfs.huggingface.co'):
+            url = url.replace('https://', 'http://')
         return super().get_connection(url, proxies)
 
-# Create a factory function that returns a Session with configured proxies
+    def add_headers(self, request, **kwargs):
+        super().add_headers(request, **kwargs)
+
+        # If there are multiple different LFS repositories, you can override the
+        # default repository address by adding X-Dragonfly-Registry header.
+        if request.url.find('example.com') != -1:
+            request.headers["X-Dragonfly-Registry"] = 'https://example.com'
+
+# Create a factory function that returns a new Session.
 def backend_factory() -> requests.Session:
     session = requests.Session()
-    # session.mount('http://', MyAdapter())
-    session.mount('https://', MyAdapter())
+    session.mount('http://', DragonflyAdapter())
+    session.mount('https://', DragonflyAdapter())
+    session.proxies = {'http': 'http://127.0.0.1:65001'}
     return session
 
 # Set it as the default session factory
 configure_http_backend(backend_factory=backend_factory)
 
-hf_hub_download(endpoint='http://huggingface.co', repo_id="timm/mobilenetv3_large_100.ra_in1k", filename="model.safetensors", proxies={'http': 'http://127.0.0.1:65001'})
+hf_hub_download(repo_id="tiiuae/falcon-rw-1b", filename="pytorch_model.bin")
--- snapshot_download_dragonfly.py
+++ snapshot_download_dragonfly.py
@@ -1,2 +1,34 @@
+import requests
+from requests.adapters import HTTPAdapter
+from urllib.parse import urlparse
 from huggingface_hub import snapshot_download
-snapshot_download(endpoint="http://huggingface.co", repo_id="gaius-qi/dragonfly", proxies={'http': 'http://127.0.0.1:65001'})
+from huggingface_hub import configure_http_backend
+
+class DragonflyAdapter(HTTPAdapter):
+    def get_connection(self, url, proxies=None):
+        # Change the schema of the LFS request to download large files from https:// to http://,
+        # so that Dragonfly HTTP proxy can be used.
+        if url.startswith('https://cdn-lfs.huggingface.co'):
+            url = url.replace('https://', 'http://')
+        return super().get_connection(url, proxies)
+
+    def add_headers(self, request, **kwargs):
+        super().add_headers(request, **kwargs)
+
+        # If there are multiple different LFS repositories, you can override the
+        # default repository address by adding X-Dragonfly-Registry header.
+        if request.url.find('example.com') != -1:
+            request.headers["X-Dragonfly-Registry"] = 'https://example.com'
+
+# Create a factory function that returns a new Session.
+def backend_factory() -> requests.Session:
+    session = requests.Session()
+    session.mount('http://', DragonflyAdapter())
+    session.mount('https://', DragonflyAdapter())
+    session.proxies = {'http': 'http://127.0.0.1:65001'}
+    return session
+
+# Set it as the default session factory
+configure_http_backend(backend_factory=backend_factory)
+
+snapshot_download(repo_id="tiiuae/falcon-rw-1b")
